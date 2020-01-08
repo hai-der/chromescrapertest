75bf94f942581698512952091600b5ae11fe68ba
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-01-08 12:31:19 -0800

    contentScript loading

---
+++ public/contentScript.js
@@ -0,0 +1,52 @@
+function gatherHouseInfo() {
+  let xhrOverrideScript = document.createElement('script');
+  xhrOverrideScript.type = "text/javascript";
+  xhrOverrideScript.innerHTML = `
+  (function() {
+    let XHR = XMLHttpRequest.prototype;
+    let send = XHR.send;
+    let open = XHR.open;
+
+    XHR.open = function(method, url) {
+      this.url = url;
+      return open.apply(this, arguments);
+    }
+
+    XHR.send = function() {
+      this.addEventListener("load", function() {
+        if(this.url.includes("https://www.realtor.com")) {
+          let dataDOMElement = document.createElement("div");
+          dataDOMElement.id = "houseItems";
+          dataDOMElement.innerText = this.response;
+          dataDOMElement.style.height = 0;
+          dataDOMElement.style.overflow = "hidden";
+          document.body.appendChild(dataDOMElement);
+        }
+      });
+      return send.apply(this, arguments);
+    };
+  })()
+  `
+  document.head.prepend(xhrOverrideScript);
+}
+
+function checkForDOM() {
+  if(document.body && document.head) {
+    gatherHouseInfo();
+  } else {
+    requestAnimationFrame(checkForDOM);
+  }
+}
+
+requestAnimationFrame(checkForDOM);
+
+function scrapeData() {
+  let responseContainingEle = document.getElementById("houseItems");
+  if(responseContainingEle) {
+    let response = JSON.parse(responseContainingEle.innerHTML);
+  } else {
+    requestAnimationFrame(scrapeData);
+  }
+}
+
+requestAnimationFrame(scrapeData);
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+const App = () => {
+  const testItem = document.getElementsByClassName("meta-value");
+  console.log(testItem);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This should be an item:
+          {/* {
+            testItem.foreach(element => {
+              return <h1>{element.textContent}</h1>
+            });
+          } */}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
