8d9e17819bfe6df4713500e2e0f4cbdec0d41cd8
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-01-15 08:02:35 -0800

    Adjust css location

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => {
     <div className="App">
       {isAuthenticated ?
         <Card>
-          <Card.Img style={{ width: "250px", height: "200px"}} variant="top" src="https://ap.rdcpix.com/1e19c9cab8dfeae300a06cc9bfb47037l-m3144314282xd-w1020_h770_q80.jpg" alt="temp pic" />
+          <Card.Img className="loginLogo" variant="top" src="https://ap.rdcpix.com/1e19c9cab8dfeae300a06cc9bfb47037l-m3144314282xd-w1020_h770_q80.jpg" alt="temp pic" />
           <Card.Body>
             <h1>This would be the address</h1>
             <h4>This would be the description</h4>
